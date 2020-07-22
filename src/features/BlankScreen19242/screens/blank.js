78590820d0b9b95d3f4e2897a289fd9acd19e1ec
Crowdbotics <team@crowdbotics.com>
2020-07-22 15:45:00 +0000

    Update components

--- src/features/BlankScreen19242/screens/blank.js
+++ src/features/BlankScreen19242/screens/blank.js
@@ -149,7 +149,7 @@ export class _Blank extends React.Component {
           textAlign: "center",
           verticalAlign: "baseline",
           fontSize: 20,
-          color: "#FFFFFF",
+          color: "#d91c1c",
           backgroundColor: "#FFFFFF",
           fontStyle: "normal",
           fontWeight: "normal",
